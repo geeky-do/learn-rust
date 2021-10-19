308900b8efd4dad61f5b3446404b7e1370987fb9
Dina Youssef <dina.youssef10@gmail.com>
2021-10-19 15:59:35 +0200

    Update main.rs
    Signed-off-by: Dina Youssef <dina.youssef10@gmail.com>

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -5,7 +5,7 @@ fn main() -> io::Result<()>{
     let mut weight_input= String::new();
 
     io::stdin().read_line(&mut weight_input)?;
-    let weight_float : f32 = weight_input.trim().parse().unwrap();
+    let weight_float : f32 = weight_input.trim().parse()?;
     println!("Your input was: {}", weight_float);
     let mars_weight : f32 = calculate_weight(weight_float);
     println!("Our weight on Mars : {}", mars_weight);
