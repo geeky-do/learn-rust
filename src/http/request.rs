0650010328bd52fb03335d71a69bf49dfba9a058
Dina Youssef <dina.youssef10@gmail.com>
2021-11-03 15:51:13 +0100

    Add Custom error handling to request
    Signed-off-by: Dina Youssef <dina.youssef10@gmail.com>

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,58 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Result as FmtResult;
+use std::str;
+
 pub struct Request {
     resource_name: String,
     query_param: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        match str::from_utf8(buf) {
+            Ok(request) => {}
+            Err(_) => {}
+        }
+        let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "InvalidRequest",
+            Self::InvalidEncoding => "InvalidEncoding",
+            Self::InvalidProtocol => "InvalidProtocol",
+            Self::InvalidMethod => "InvalidMethod",
+        }
+    }
+}
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
