b604880e2f66c123277f9ef2af2da423f688aeec
Dina Youssef <dina.youssef10@gmail.com>
2021-11-15 15:43:17 +0100

    Add new type StatusCode
    Signed-off-by: Dina Youssef <dina.youssef10@gmail.com>

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,9 +1,9 @@
+use super::status_code::StatusCode;
+
 pub struct Response {
     status_code: StatusCode,
     body: Option<String>,
 }
-
-pub enum StatusCode {}
 
 impl Response {
     pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
