dc6384311d8697a40bf813a481dd901e1d115b86
Dina Youssef <dina.youssef10@gmail.com>
2021-11-16 13:49:05 +0100

    Add routing to handler
    Signed-off-by: Dina Youssef <dina.youssef10@gmail.com>

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,10 +1,15 @@
-use super::http::{ParseError, Request, Response, StatusCode};
+use super::http::{Method, ParseError, Request, Response, StatusCode};
 use super::server::Handler;
 pub struct RequestHandler;
 
 impl Handler for RequestHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         dbg!(request);
-        Response::new(StatusCode::Ok, Some("<h1>Hiii</h1>".to_string()))
+        match request.method() {
+            Method::GET => Response::new(StatusCode::Ok, Some("<h1>GET</h1>".to_string())),
+            Method::POST => Response::new(StatusCode::Ok, Some("<h1>POST</h1>".to_string())),
+            Method::DELETE => Response::new(StatusCode::Ok, Some("<h1>DELETE</h1>".to_string())),
+            Method::PUT => Response::new(StatusCode::Ok, Some("<h1>PUT</h1>".to_string())),
+        }
     }
 }
